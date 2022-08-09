1c77ffd44748f4abf044f319131552fe53b81ba7
MaxShady <max.shady1611@gmail.com>
2022-08-10 02:27:24 +0430

    Add tests for pharmacist

---
+++ tests/unit/staff/pharmacist/CreatePharmacist.spec.ts
@@ -0,0 +1,42 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { pharmacist } from "#modules/staff/StaffModule";
+import { v4 as uuidv4 } from "uuid";
+import {
+    CreatePharmacistCommandHandler
+} from "#modules/staff/pharmacist/commands/create-pharmacist/command/CreatePharmacistCommandHandler";
+import { PharmacistRepository } from "#modules/staff/pharmacist/infrastructure/PharmacistRepository";
+import { PharmacistDiTokens } from "#libs/tokens/PharmacistDiTokens";
+import { Pharmacist } from "#modules/staff/pharmacist/domain/PharmacistEntity";
+
+describe("CreatePharmacist", () => {
+    const mockId = uuidv4();
+    let createPharmacistCommandHandler: CreatePharmacistCommandHandler;
+    let pharmacistRepository: PharmacistRepository;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                ...pharmacist.sharedProviders,
+                ...pharmacist.providers.createPharmacist,
+            ]
+        }).compile();
+        createPharmacistCommandHandler = module.get(CreatePharmacistCommandHandler);
+        pharmacistRepository = module.get(PharmacistDiTokens.pharmacistRepository);
+    });
+
+    it('should create an accountant', async () => {
+        jest.spyOn(pharmacistRepository, "create")
+            .mockImplementation(async (pharmacist: Pharmacist) => {
+                jest.spyOn(pharmacist, "getId").mockReturnValue(mockId);
+                return {
+                    id: pharmacist.getId()
+                }
+            });
+        const pharmacist = await createPharmacistCommandHandler.execute({
+            name: "John Doe",
+            password: "12345678"
+        });
+
+        expect(pharmacist.id).toEqual(mockId)
+    });
+});
---
+++ tests/unit/staff/pharmacist/GetPharmacist.spec.ts
@@ -0,0 +1,48 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { pharmacist } from "#modules/staff/StaffModule";
+import { v4 as uuidv4 } from "uuid";
+import {
+    GetPharmacistQueryHandler
+} from "#modules/staff/pharmacist/queries/get-pharmacist/query/GetPharmacistQueryHandler";
+import { PharmacistRepository } from "#modules/staff/pharmacist/infrastructure/PharmacistRepository";
+import { PharmacistDiTokens } from "#libs/tokens/PharmacistDiTokens";
+import { Pharmacist } from "#modules/staff/pharmacist/domain/PharmacistEntity";
+
+describe("GetPharmacist", () => {
+    const mockId = uuidv4();
+    let getPharmacistQueryHandler: GetPharmacistQueryHandler;
+    let pharmacistRepository: PharmacistRepository;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                ...pharmacist.sharedProviders,
+                ...pharmacist.providers.getPharmacist
+            ]
+        }).compile();
+
+        getPharmacistQueryHandler = module.get(GetPharmacistQueryHandler);
+        pharmacistRepository = module.get(PharmacistDiTokens.pharmacistRepository);
+    });
+
+    it('should get an accountant', async () => {
+        jest.spyOn(pharmacistRepository, "find")
+            .mockImplementation(async (id: string) => {
+                return Pharmacist.loadExisting({
+                    id: id,
+                    name: "John Doe",
+                    password: "12345678",
+                    joinedAt: new Date(),
+                    updatedAt: null,
+                    currentPatientCode: null,
+                    todaySales: null
+                });
+            });
+
+        const accountant = await getPharmacistQueryHandler.execute({
+            id: mockId
+        });
+
+        expect(accountant.name).toEqual("John Doe");
+    });
+});
