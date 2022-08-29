16c76d4a9f4b6e2de8c63f42a2998b6af0970bc2
MaxShady <max.shady1611@gmail.com>
2022-08-30 02:02:58 +0430

    Time to sleep

--- src/modules/inventory/commands/update-drug-quantity/UpdateDrugQuantityCommandHandler.ts
+++ src/modules/inventory/commands/update-drug-quantity/UpdateDrugQuantityCommandHandler.ts
@@ -14,8 +14,15 @@ export class UpdateDrugQuantityCommandHandler implements ICommandHandler<UpdateD
     ) {}
 
     public async execute(command: UpdateDrugQuantityCommand): Promise<void> {
+        await this.throwIfDrugNotFound(command.drugId);
         const drug = await this.inventoryRepository.findById(command.drugId);
         drug.increaseQuantity(command.quantity);
         await this.inventoryRepository.update(drug);
+    }
+
+    private async throwIfDrugNotFound(id: string) {
+        const count = await this.inventoryRepository.count(id);
+        if (count === 0)
+            throw new Error("Drug not found");
     }
 }
