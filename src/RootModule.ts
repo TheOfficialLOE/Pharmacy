f629aae1e8ef84dd55751b9d0defb066adebbad3
MaxShady <max.shady1611@gmail.com>
2022-08-30 23:59:39 +0430

    Create PatientServiceModule

--- src/RootModule.ts
+++ src/RootModule.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { IdentityAndAccessModule } from "#modules/identity-and-access/IdentityAndAccessModule";
 import { InventoryModule } from "#modules/inventory/InventoryModule";
 import { InfrastructureModule } from "#infrastructure/InfrastructureModule";
+import { PatientServiceModule } from "#modules/patient-service/PatientServiceModule";
 
 @Module({
     imports: [
         InfrastructureModule,
         IdentityAndAccessModule,
         InventoryModule,
+        PatientServiceModule,
     ],
 })
 export class RootModule {
