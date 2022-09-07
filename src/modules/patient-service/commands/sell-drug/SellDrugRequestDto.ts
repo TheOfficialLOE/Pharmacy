26a8c314cff09cbd477d7bc7771b8cbe15467f5b
MaxShady <max.shady1611@gmail.com>
2022-09-07 18:14:43 +0430

    Add request dto for selling drug to patient

---
+++ src/modules/patient-service/commands/sell-drug/SellDrugRequestDto.ts
@@ -0,0 +1,22 @@
+import { IsDefined, IsNumber, IsString, Length, Min, ValidateNested } from "class-validator";
+import { Type } from "class-transformer";
+
+export class DemandedDrug {
+    @IsString()
+    @IsDefined()
+    public readonly drugId: string;
+
+    @IsNumber()
+    @Min(1)
+    public readonly quantity: number;
+}
+
+export class SellDrugRequestDto {
+    @IsString()
+    @Length(4)
+    public readonly patientCode: string;
+
+    @ValidateNested({ each: true })
+    @Type(() => DemandedDrug)
+    public readonly demandedDrugs: DemandedDrug[];
+}
